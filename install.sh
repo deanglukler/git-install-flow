a072d873c552cd5a334a668ca64041fb6e53d8ef
dean glükler <glueckler@gmail.com>
2024-03-27 17:20:43 -0600

    more or less...

--- install.sh
+++ install.sh
@@ -1,1 +1,10 @@
-echo "hi"
+DIRECTOR="$HOME/.config/fakeshit"
+
+read -p "REPLACING $DIRECTOR -> Enter or Ctrl+C to cancel"
+
+if [ -d "$DIRECTOR" ]; then
+    echo "Deleting $DIRECTOR"
+    rm -rf "$DIRECTOR"
+fi
+
+git clone git@github.com:deanglukler/karabiner.git $DIRECTOR
